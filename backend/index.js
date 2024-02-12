88b96996d881bec95d3921aa7a32ae78b6a1c7ec
Sanket-777 <Ksanket114402@gmail.com>
2024-02-12 17:27:45 +0530

    added welcome message
    i

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 // backend/index.js
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const mainRouter = require("./routes/index");
-
 
 const app = express();
 
-app.use(cors()) // adding cross origin
-app.use(express.json()) // adding body parser 
+app.use(cors()); // adding cross origin
+app.use(express.json()); // adding body parser
 
+app.get("/", (req, res) => {
+  res.json("hello");
+});
 
 app.use("/api/v1", mainRouter); // routing everthing coming from this /api/v1   to mainRouter
-console.log("Server is Running  !")
+console.log("Server is Running  !");
 app.listen(3000);
